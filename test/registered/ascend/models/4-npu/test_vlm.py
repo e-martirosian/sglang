c8dfcb1427f90b9c829a208f57b867166a71ac14
Elizaveta Martirosian <elizabet3000@mail.ru>
2026-04-01 14:35:38 +0300

    Update test_vlm.py

--- test/registered/ascend/models/4-npu/test_vlm.py
+++ test/registered/ascend/models/4-npu/test_vlm.py
@@ -1,5 +1,5 @@
 import unittest
-from types import SimpleNameSpace
+from types import SimpleNamespace
 
 from sglang.test.ascend.lm_utils import TestLMModels
 from sglang.test.ascend.test_ascend_utils import JANUS_PRO_1B_WEIGHTS_PATH
@@ -7,17 +7,7 @@ from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
 
-MODELS = [
-    SimpleNameSpace(
-        model=JANUS_PRO_1B_WEIGHTS_PATH,
-        mmmu_accuracy=0.2,
-        mmmu_perf=62,
-        task="mmmu_val",
-        dataset="mmmu",
-        server=SimpleNameSpace(tp=4),
-        llms_eval_args=[],
-        bench_serving_args=["--num-prompts", "2"],
-        server_args=[
+COMMON_SERVER_ARGS = [
             "--tp-size",
             "4",
             "--trust-remote-code",
@@ -29,7 +19,23 @@ MODELS = [
             "--attention-backend",
             "ascend",
             "--disable-cuda-graph",
-        ],
+        ]
+
+COMMON_MODEL_ARGS = {
+    "tp": 4,
+    "task": "mmmu_val",
+    "dataset": "mmmu",
+}
+
+MODELS = [
+    SimpleNamespace(
+        model_name=JANUS_PRO_1B_WEIGHTS_PATH,
+        accuracy=0.2,
+        out_throughput=62,
+        llms_eval_args=[],
+        bench_serving_args=["--num-prompts", "2"],
+        server_args=COMMON_SERVER_ARGS,
+        **COMMON_MODEL_ARGS
     )
 ]
 
@@ -37,6 +43,9 @@ MODELS = [
 class TestVLMModels(TestLMModels):
     models = MODELS
 
+    def test_vlm_mmmu_benchmakr(self):
+        for model in self.model_names:
+            self._run_vlm_mmmu_test(model, "./logs")
 
 if __name__ == "__main__":
     unittest.main()
