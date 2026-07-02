d5f34230f6069418471f59ecdb1c2f4b7b9bdeda
Elizaveta Martirosian <elizaveta.martirosian@gmail.com>
2026-07-02 21:23:06 +0300

    update

--- python/sglang/srt/models/llama.py
+++ python/sglang/srt/models/llama.py
@@ -57,6 +57,9 @@ from sglang.srt.server_args import get_global_server_args
 from sglang.srt.utils import add_prefix, is_cuda, is_npu, is_xpu, make_layers
 from sglang.utils import get_exception_traceback
 
+from transformers.models.mistral.modeling_mistral import (
+    MistralRotaryEmbedding,
+)
 
 _is_cuda = is_cuda()
 _is_xpu = is_xpu()
@@ -195,6 +198,8 @@ class LlamaAttention(nn.Module):
             rope_scaling=rope_scaling,
             is_neox_style=rope_is_neox_style,
         )
+        self.rotary_emb = MistralRotaryEmbedding(config=config)
+
         self.attn = RadixAttention(
             self.num_heads,
             self.head_dim,
