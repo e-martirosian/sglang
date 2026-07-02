030266f9f771cf85f2ddba35c1b58d4cd24a4f95
Elizaveta Martirosian <elizaveta.martirosian@gmail.com>
2026-07-02 21:04:52 +0300

    update

--- python/sglang/srt/models/ministral3.py
+++ python/sglang/srt/models/ministral3.py
@@ -77,9 +77,9 @@ class Ministral3Attention(LlamaAttention):
         qkv, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
 
-        logger.info(q.dispatch())
-        logger.info(k.dispatch())
-        logger.info(v.dispatch())
+        logger.info(q)
+        logger.info(k)
+        logger.info(v)
 
         # Apply RoPE
         q, k = self.rotary_emb(positions, q, k)
